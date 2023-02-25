ebd587c5e3885b7e74fa450742ca5120aba1ac20
nishitmistry <nishitmistry94@gmail.com>
2023-02-26 00:59:20 +0530

    fully res nav

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function sidebarCollapse() {
-
   var v = document.getElementById("check");
-  var height = screen.height;
+  let height = screen.height;
   if (height < 650) {
     if (v.checked) {
       document.getElementById("sidebar").style.left = "0px";
@@ -18,6 +17,13 @@ function sidebarCollapse() {
       document.getElementById("sidebar").style.left = "-145px";
     }
   }
-
-
+}
+function navbarCollapse(){
+  var v=document.getElementById("nav-check");
+  if(v.checked){
+    document.getElementById("nav-650-650").style.top="0px";
+  }
+  else if(!v.checked){
+    document.getElementById("nav-650-650").style.top="-300px";
+  }
 }
