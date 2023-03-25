6b4d165f6e21c7882afcb46aa6628024710f886f
nishitmistry <nishitmistry94@gmail.com>
2023-03-25 07:13:01 +0530

    js 30%

--- script.js
+++ script.js
@@ -36,67 +36,116 @@ function navbarCollapse(a){
     document.getElementById("nav-650-650").style.zIndex="1";
   }
 }
-function internet(){
-  var agents = {
-    "gekko": "e370fa57-4757-3604-3648-499e1f642d3f",
-    "fade": "dade69b4-4f5a-8528-247b-219e5a1facd6",
-    "breach": "5f8d3a7f-467b-97f3-062c-13acf203c006",
-    "raze": "f94c3b30-42be-e959-889c-5aa313dba261",
-    "chamber": "22697a3d-45bf-8dd7-4fec-84a9e28c69d7",
-    "kay/o": "601dbbe7-43ce-be57-2a40-4abd24953621",
-    "skye": "6f2a04ca-43e0-be17-7f36-b3908627744d",
-    "cypher": "117ed9e3-49f3-6512-3ccf-0cada7e3823b",
-    "sova": "320b2a48-4d9b-a075-30f1-1f93a9b638fa",
-    "killjoy": "1e58de9c-4950-5125-93e9-a0aee9f98746",
-    "harbor": "95b78ed7-4637-86d9-7e41-71ba8c293152",
-    "viper": "707eab51-4836-f488-046a-cda6bf494859",
-    "phoenix": "eb93336a-449b-9c1b-0a54-a891f7921d69",
-    "astra": "41fb69c1-4189-7b37-f117-bcaf1e96f1bf",
-    "brimstone": "9f0d8ba9-4140-b941-57d3-a7ad57c6b417",
-    "neon": "bb2a4828-46eb-8cd1-e765-15848195d751",
-    "yoru": "7f94d92c-4234-0a36-9646-3a87eb8b5c89",
-    "sage": "569fdd95-4d10-43ab-ca70-79becc718b46",
-    "reyna": "a3bfb853-43b2-7238-a4f1-ad90e9e46bcc",
-    "omen": "8e253930-4c05-31dd-1b6c-968525494517",
-    "jett": "add6443a-41bd-e414-f6ad-e58d267f4e95"
-  }
-  var keys = ["gekko",
-    "fade",
-    "breach",
-    "raze",
-    "chamber",
-    "kay/o",
-    "skye",
-    "cypher",
-    "sova",
-    "killjoy",
-    "harbor",
-    "viper",
-    "phoenix",
-    "astra",
-    "brimstone",
-    "neon",
-    "yoru",
-    "sage",
-    "reyna",
-    "omen",
-    "jett",]
 
-    fetch('https://valorant-api.com/v1/agents/'+agents[getRandomKey(keys)])
+var agents = {
+  "gekko": "e370fa57-4757-3604-3648-499e1f642d3f",
+  "fade": "dade69b4-4f5a-8528-247b-219e5a1facd6",
+  "breach": "5f8d3a7f-467b-97f3-062c-13acf203c006",
+  "raze": "f94c3b30-42be-e959-889c-5aa313dba261",
+  "chamber": "22697a3d-45bf-8dd7-4fec-84a9e28c69d7",
+  "kay/o": "601dbbe7-43ce-be57-2a40-4abd24953621",
+  "skye": "6f2a04ca-43e0-be17-7f36-b3908627744d",
+  "cypher": "117ed9e3-49f3-6512-3ccf-0cada7e3823b",
+  "sova": "320b2a48-4d9b-a075-30f1-1f93a9b638fa",
+  "killjoy": "1e58de9c-4950-5125-93e9-a0aee9f98746",
+  "harbor": "95b78ed7-4637-86d9-7e41-71ba8c293152",
+  "viper": "707eab51-4836-f488-046a-cda6bf494859",
+  "phoenix": "eb93336a-449b-9c1b-0a54-a891f7921d69",
+  "astra": "41fb69c1-4189-7b37-f117-bcaf1e96f1bf",
+  "brimstone": "9f0d8ba9-4140-b941-57d3-a7ad57c6b417",
+  "neon": "bb2a4828-46eb-8cd1-e765-15848195d751",
+  "yoru": "7f94d92c-4234-0a36-9646-3a87eb8b5c89",
+  "sage": "569fdd95-4d10-43ab-ca70-79becc718b46",
+  "reyna": "a3bfb853-43b2-7238-a4f1-ad90e9e46bcc",
+  "omen": "8e253930-4c05-31dd-1b6c-968525494517",
+  "jett": "add6443a-41bd-e414-f6ad-e58d267f4e95"
+}
+var keys = ["gekko",
+  "fade",
+  "breach",
+  "raze",
+  "chamber",
+  "kay/o",
+  "skye",
+  "cypher",
+  "sova",
+  "killjoy",
+  "harbor",
+  "viper",
+  "phoenix",
+  "astra",
+  "brimstone",
+  "neon",
+  "yoru",
+  "sage",
+  "reyna",
+  "omen",
+  "jett",]
+function setMainAgent(agentName){
+    fetch('https://valorant-api.com/v1/agents/'+agents[agentName])
     .then(response => response.json())
     .then(json => {
-      // document.getElementById("character-container").setAttribute("src", json["data"]["fullPortrait"])
+      document.getElementById("character-image").setAttribute("src", json["data"]["fullPortrait"])
       var ability_logo=document.getElementsByClassName("ability-logo")
       count=0
-      for (var i of ability_logo){
+      for (let i of ability_logo){
         i.setAttribute("src",json["data"]["abilities"][count]["displayIcon"])
         count+=1
       }
+      document.getElementById("agent-name").innerHTML=json["data"]["displayName"].toUpperCase()
 
     })
   }
-  internet()
+function setMatchHistoryAgent(agentNames){
+    let count=0
+    // var a=document.getElementsByClassName("match-agent-img")
+    for (let i=0; i<agentNames.length; i++){
+      fetch('https://valorant-api.com/v1/agents/'+agents[agentNames[i]])
+      .then(response => response.json())
+      .then(json => {
+        var agent_hist_img=document.getElementsByClassName("match-agent-img")[i]
+        agent_hist_img.setAttribute("src",json["data"]["displayIcon"])
+      });
+    }
+}
+function setStatValue(statValues){
+  let c=0
+  // var a=document.getElementsByClassName("match-agent-img")
+
+  for (let i=0; i<statValues.length; i++){
+    let stats_div=document.getElementsByClassName("stats-value")[c]
+    console.log(stats_div)
+    stats_div.innerHTML=statValues[c]
+    c+=1
+  }
+}
+
+
   function getRandomKey(keys) {
 
     return keys[Math.floor(Math.random() * keys.length)];
   }
+  var unrated={
+    "main-agent":"gekko",
+    "agent-played-time":"Played 164Hrs",
+    "stats-value":["1.2","130.5","60%","40%"],
+    "ability-kills-value":["0.76","0.53","0.2"],
+    "match-score-win":["13","10","2","13"],
+    "match-score-loss":["7","13","13","5"],
+    "match-kda-value":["12/3/4","24/7/9","5/6/8","31/13/4"],
+    "match-map":["IceBox","Haven","Split","Haven"],
+    "rating-img":["d2","d1","d1","d1"],
+    "match-agent-img":["breach","fade","sova","reyna"]
+  }
+  function setContent(){}
+
+  function changePage(mode){
+    
+    if (mode=="ur")
+    {
+
+    }
+  }
+setMatchHistoryAgent(unrated["match-agent-img"])
+setMainAgent(unrated["main-agent"])
+setStatValue(unrated["stats-value"])
